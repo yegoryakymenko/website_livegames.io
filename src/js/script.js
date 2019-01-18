3e726afcac86804dc389a56d273ce3d67913ecdf
maria <makifieva@livegames.io>
2019-01-18 13:40:02 +0200

    part of vidio opt(saving)

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+//ADD COROUSEL
 var swiperGames = new Swiper('.our-games__container', {
   slidesPerView: 3,
   spaceBetween: 70,
@@ -20,3 +21,45 @@ var swiperTombala = new Swiper('.tombala__container', {
     clickable: true,
   },
 });
+//OPTIMIZE VIDEO
+
+document.addEventListener("DOMContentLoaded", lazyVideoLoad);
+document.addEventListener("scroll", lazyVideoLoad);
+
+function lazyVideoLoad() {
+  var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
+
+  if ("IntersectionObserver" in window) {
+    var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(video) {
+        console.log("video.isIntersecting",video.isIntersecting);
+        console.log(video.target);
+        if (video.isIntersecting) {
+          video.target.play(); 
+        }else{
+          video.target.pause();
+        }
+        // if (video.isIntersecting) {
+        //   for (var source in video.target.children) {
+        //     var videoSource = video.target.children[source];
+        //     if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+        //       videoSource.src = videoSource.dataset.src;
+        //     }
+        //   }
+        //
+        //   video.target.load();
+        //   video.target.classList.remove("lazy");
+        //   lazyVideoObserver.unobserve(video.target);
+        // }
+      });
+    });
+
+    lazyVideos.forEach(function(lazyVideo) {
+      lazyVideoObserver.observe(lazyVideo);
+    });
+  }
+}
+// SMOOTH ANCHOR TRANSITION?
+// document.querySelector('.about-us').scrollIntoView({
+//   behavior: 'smooth'
+// });
